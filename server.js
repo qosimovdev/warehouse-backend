b1d1cef0f6789d0561e903fee8cc297957ebbfc7
ibrohim3 <ibrohim.2128382@gmail.com>
2026-02-06 19:24:43 +0500

    done

--- server.js
+++ server.js
@@ -9,13 +9,36 @@
 //     console.log(`Server running on port http://localhost:${PORT}`);
 // })
 
+// require("dotenv").config()
+// const app = require("./src/app")
+// const connectDB = require("./src/config/db")
+
+// connectDB()
+
+// const PORT = process.env.PORT || 7777
+// app.listen(PORT, () => {
+//     console.log(`Server running on port http://localhost:${PORT}`)
+// })
+
+
 require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require("./src/config/db")
 
-connectDB()
-
 const PORT = process.env.PORT || 7777
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`)
-})
+
+async function startServer() {
+    try {
+        await connectDB()
+        console.log("Mongo DB is connected")
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    } catch (err) {
+        console.error("Startup error:", err.message)
+        // ❗ process.exit YO‘Q
+    }
+}
+
+startServer()
--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,26 @@
+// const mongoose = require("mongoose")
+
+// const connectDB = async () => {
+//     try {
+//         console.log("MONGO_URI:", process.env.MONGO_URI);
+//         await mongoose.connect(process.env.MONGO_URI)
+//         console.log("Mongo DB is connected");
+//     } catch (err) {
+//         console.error("Mongo DB connection faild:", err.message)
+//         process.exit(1)
+//     }
+// }
+
+// module.exports = connectDB
+
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-    try {
-        console.log("MONGO_URI:", process.env.MONGO_URI);
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Mongo DB is connected");
-    } catch (err) {
-        console.error("Mongo DB connection faild:", err.message)
-        process.exit(1)
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is not defined")
     }
+
+    await mongoose.connect(process.env.MONGO_URI)
 }
 
 module.exports = connectDB
