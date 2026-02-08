6644146515f22c20782f583f693c577deac911f5
ibrohim3 <ibrohim.2128382@gmail.com>
2026-02-08 22:46:05 +0500

    fix: start server using process.env.PORT for Railway

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require("./src/config/db")
 
-const PORT = process.env.PORT || 7777
+const PORT = process.env.PORT || 3000
 
 async function startServer() {
     try {
